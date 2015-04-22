0f1183e2485e002971ddeb846af9bd717cec44d5
Maenox <k7.blooms@gmail.com>
2015-04-22 17:11:31 +0900

    Cとか押すと終了します。

--- serialAX.py
+++ serialAX.py
@@ -1,20 +1,29 @@
 #coding: utf-8
 #AX2PCの通信
-import sys
+CR = ' '
+LF = ' '
+ESC = 'c'
+TIMEOUT = 0.5
+
+import mscvrt
 import serial
 import time
-#COM2を開く。ポートは0から数えるので通常COM1は0番目のポートである
-#com01 = serial.Serial(2, timeout=1)
+
+#COM4を開く、AXとの通信
 com02 = serial.Serial(3, timeout=1)
 
 #COM1に書きこむ
 com02.write('SHIFT 20,10,0,0,0,0\r')
 
 while True:
+    if msvcrt.kbhit():
+        inp = msvcrt.getch()
+        if(inp == ESC):
+            break
     str = com02.read(30)
     print str
+    time.sleep(1)
 
 #終わったらclose
 #続けて書きこむ場合はいちいちcloseしなくてもいい
-com01.close()
 com02.close()
--- serialFH.py
+++ serialFH.py
@@ -1,8 +1,15 @@
 #coding: utf-8
-#AX2PCの通信
+#FH2PCの通信
+CR = ' '
+LF = ' '
+ESC = 'ESC'
+TIMEOUT = 0.5
+
 import sys
+import msvcrt
 import serial
 import time
+
 #COM2を開く。ポートは0から数えるので通常COM1は0番目のポートである
 com01 = serial.Serial(2, timeout=1)
 #com02 = serial.Serial(3, timeout=1)
@@ -11,12 +18,14 @@ com01 = serial.Serial(2, timeout=1)
 com01.write('SHIFT 20,10,0,0,0,0\r')
 
 while True:
+    if msvcrt.kbhit():
+        inp = msvcrt.getch()
+        if(inp == ESC):
+            break
     str = com01.read(30)
     print str
-    time.sleep(10)
-
+    time.sleep(1)
 
 #終わったらclose
 #続けて書きこむ場合はいちいちcloseしなくてもいい
 com01.close()
-com02.close()
