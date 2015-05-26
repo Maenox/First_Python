b28756c64636fa7eb9999bac0e5c10195875328f
Maenox <k7.blooms@gmail.com>
2015-05-26 10:10:49 +0900

    裏面対応

--- serialFH.py
+++ serialFH.py
@@ -1,55 +1,56 @@
 CR = '\n'
 LF = '\r'
 ESC = 'c'
-TIMEOUT = 0.5
 
 import sys
 import msvcrt
 import serial
 import time
+import string
 
-# COM3 open serial to FH
+# COM2 open serial to FH
 comFH = serial.Serial(
-    port=1,
-    baudrate=19200,
-    bytesize=7,
-    stopbits=1
-    timeout=0)
-
-# COM4 open serial to AX
-comAX = serial.Serial(
     port=0,
+    baudrate=38400,
+    bytesize=8,
+    stopbits=1,
+    timeout=1)
+
+# COM1 open serial to AX
+comAX = serial.Serial(
+    port=5,
     baudrate=19200,
     bytesize=7,
     stopbits=1,
-    timeout=5)  # COM4
+    timeout=1)
 
 while True:
     # C key is exit
     if msvcrt.kbhit():
         inp = msvcrt.getch()
         if(inp == ESC):
+            comFH.flush()
+            comAX.flush()
             comFH.close()
             comAX.close()
             break
 
-    strAX = comAX.read(30)
-    strFH = comFH.read(30)
+    strAX = comAX.read(20)
+    strFH = comFH.read(128)
     # if command from AX
     if(strAX.find('SHIFT 1') == 0):
-        comFH.write('MEASURE /C\r')  # start measure
-        strFH = comFH.read(3)  # read FH
+        comFH.write('MEASURE\r')  # start measure
+        strFH = comFH.read(128)  # read F
         if(strFH.find('OK') == 0):
-            strFH.read(30)
-            comAX.write('SHIFT' + str(strFH) + '\r')
-            print 'Success SHIFT Command!!'
-            comAX.write('MEASURE /E\r')  # Stop Measure
-        else:
-
-            print 'Bad MEASURE...'
-        print str(strFH)
+            comAX.write('SHIFT ' + strFH[4:60] + '\n')
+            print 'Success SHIFT 1 Command!!'
+    elif(strAX.find('SHIFT 2') == 0):
+        comFH.write('MEASURE\r')  # start measure Backprint
+        strFH = comFH.read(128)
+        if(strFH.find('OK') == 0):
+            comAX.write('SHIFT ' + strFH[57:] + '\n')
+            print 'Success SHIFT 2 Command!!'
     else:
         strAX = strFH
         comFH.write(strAX)
-        #print 'command is not SHIFT'
-        print strAX
+        #  print strAX
